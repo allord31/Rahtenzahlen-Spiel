726d3aa0aca10006a8c16665b75cbf49fa46d79f
allord31 <75315127+allord31@users.noreply.github.com>
2020-12-01 16:39:09 +0100

    Create Zahlraten-spiel.py

---
+++ Zahlraten-spiel.py
@@ -0,0 +1,12 @@
+import random
+number = random.randrange(1,100)
+raten = int(input("geben Sie ein Nummer Zwichen 1,100 ein:  "))
+while raten != number:
+    if raten > number:
+        print("geben Sie eine kleine Nummer \n")
+        raten= int (input("- versuchen Sie noch einmal : "))
+    else:
+        print("geben Sie größe Nummer,und versuch noch ein mal\n")
+        raten =int (input("- versuchen Sie richtig zu Raten : "))
+
+print("Glückwünsch !! Sie haben gewonen .")
