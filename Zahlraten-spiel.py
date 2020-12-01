234ee7590661f5026e16d3e3817f1231ab7cbfd7
allord31 <75315127+allord31@users.noreply.github.com>
2020-12-01 18:13:06 +0100

    Update Zahlraten-spiel.py

--- Zahlraten-spiel.py
+++ Zahlraten-spiel.py
@@ -1,12 +1,17 @@
 import random
 number = random.randrange(1,100)
-raten = int(input("geben Sie eine Nummer Zwichen 1 und 100 ein:  "))
+raten = int(input("geben Sie eine Nummer Zwischen 1 und 100 ein: "))
+
 while raten != number:
-    if raten > number:
-        print("geben Sie eine kleine Nummer \n")
-        raten= int (input("- versuchen Sie noch einmal : "))
+    if     raten  <=0 or raten >=101  :
+        raten = int(input("Die eingegebene Nummer ist entwieder großer als 100 oder kleiner als 1 "
+                          "\n bitte versuchen Sie eine Nummer Zwischen 1 bis 100 einzugeben  "))
+    elif  raten > number:
+        print("geben Sie eine kleinere Nummer \n")
+        raten= int (input("- versuchen Sie noch ein Mal : "))
     else:
-        print("geben Sie größe Nummer,und versuch noch ein mal\n")
+        print("geben Sie größere Nummer,und versuch noch ein Mal\n")
         raten =int (input("- versuchen Sie richtig zu Raten : "))
 
 print("Glückwünsch !! Sie haben gewonnen .")
+
